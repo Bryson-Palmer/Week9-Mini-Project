503809e658ad733cc0915e32da59d34d4a992717
Tskading <Tskading@gmail.com>
2020-11-30 21:57:39 -0800

    built template literal

--- 28-Stu_Mini-project/index.js
+++ 28-Stu_Mini-project/index.js
@@ -1,16 +1,64 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+const writeFileAsync = util.promisify(fs.writeFile);
+// look up line above to learn more :)
+const prompts = () => 
+    
+    inquirer.prompt([
 
-inquirer.prompt([
+        {  
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
 
-    {  
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
+        {
+            type: 'input',
+            name: 'location',
+            message: 'Where are you from?',
+        },
 
-    {
-        type: ''
-    }
+        // {
+        //     type:
+        // }
 
 ])
+const html = (data) => `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <title>Document</title>
+    </head>
+    <body>
+    <div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h1 class="display-4">Hi! My name is ${data.name}</h1>
+        <p class="lead">I am from ${data.location}.</p>
+        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+        <ul class="list-group">
+        <li class="list-group-item">My GitHub username is </li>
+        <li class="list-group-item">LinkedIn: </li>
+        </ul>
+    </div>
+    </div>
+    </body>
+    </html>`
+
+prompts()
+    .then((data) => { writeFileAsync("index.html", html(data))
+
+    .then
+    
+    
+        
+    // console.log(data);
+
+   
+
+    })
+
+// console.log(prompts);
+// console.log(html);
